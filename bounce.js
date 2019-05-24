c6fa467f81fb369f8479be8402d242a5bf77a0a6
vincentharkins <50749068+vincentharkins@users.noreply.github.com>
2019-05-24 10:31:51 -0700

    Update bounce.js

--- bounce.js
+++ bounce.js
@@ -1,6 +1,6 @@
 let bubbles = []
 function setup() {
-  createCanvas(displayWidth- 300, displayHeight-300);
+  createCanvas(displayWidth- 150, displayHeight-200);
   for (let i = 0;i < 600;i++){
     let x = 20 + 100 * i;
     bubbles[i] = new Bubble(x,200,200)
